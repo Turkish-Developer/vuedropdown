16b5fec9c276e819cd135c51cffe9a246d335cec
senesatka <senesatka@gmail.com>
2024-06-05 14:40:00 +0300

    Dropdown Button yapıldı

---
+++ src/components/DropdownButton.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps<{
+  width?: string;
+  btnColor?: string;
+  class?: string;
+  radius?: string;
+  id: string;
+}>();
+</script>
+<template>
+  <button
+    :id="`td-dropdown-button-${props.id}`"
+    :class="`text-white cursor-pointer h-10 w-24 flex bg-none justify-center items-center gap-2 transition-all duration-500 ease-in-out rounded-lg border-solid border  ${$props.class}`"
+    :style="`background-color:${btnColor ?? 'rgb(55,65,81)'};border-radius:${
+      radius ?? '8px'
+    }`"
+  >
+    <slot />
+  </button>
+</template>
+<style></style>
