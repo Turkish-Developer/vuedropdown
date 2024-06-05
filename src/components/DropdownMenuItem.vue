b92fc35a65c88a6d7fff390526238bd8ef0a54d0
senesatka <senesatka@gmail.com>
2024-06-05 14:40:28 +0300

    Dropdown Menu Item yapıldı

---
+++ src/components/DropdownMenuItem.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ btnColor?: string; radius?: string; class?: string }>();
+</script>
+<template>
+  <div
+    :class="`w-full min-h-6 cursor-pointer hover:bg-[rgb(80,90,106)] flex justify-start p-2 items-center gap-2.5 text-gray-400 transition-all duration-500 ease-in-out hover:text-gray-300 ${$props.class}`"
+    :style="`background-color:${btnColor ?? 'none'};border-radius:${
+      radius ?? '8px'
+    }`"
+  >
+    <slot />
+  </div>
+</template>
+<style scoped></style>
