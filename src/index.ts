7c7723816b7fb77ec220027a8b58b589bbde261c
senesatka <senesatka@gmail.com>
2024-06-05 14:38:57 +0300

    Temel

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import DropdownButton from "./components/DropdownButton.vue";
+import DropdownMenu from "./components/DropdownMenu.vue";
+import DropdownMenuItem from "./components/DropdownMenuItem.vue";
+import "./index.css";
+export { DropdownMenuItem, DropdownButton, DropdownMenu };
